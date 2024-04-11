4ceba264a2332cb5644b9f73185bbe5a0a315b24
darriusdacquel <dacqueld@hawaii.edu>
2024-04-10 20:41:33 -1000

    Landing page with no buttons yet

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -3,9 +3,9 @@ import { Col, Container, Row } from 'react-bootstrap';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
-  <Container id="landing-page" fluid className="py-3">
-    <Row className="align-middle text-center">
-      <Col xs={8} className="d-flex flex-column justify-content-center">
+  <Container id="landing-page" fluid className="py-3 d-flex h-100">
+    <Row className="justify-content-center align-items-center w-100">
+      <Col xs={12} md={8} className="d-flex flex-column justify-content-center align-items-center text-center">
         <h1>Welcome!</h1>
         <p>Please select an action below.</p>
       </Col>
