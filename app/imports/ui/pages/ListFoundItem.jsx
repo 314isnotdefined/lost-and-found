d886845c7b58971876adc2cbd8d84f72f209e65a
Samdoann <samdoan808@gmail.com>
2024-04-11 11:48:18 -1000

    editing List found items page
    not done

--- app/imports/ui/pages/ListFoundItem.jsx
+++ app/imports/ui/pages/ListFoundItem.jsx
@@ -1,52 +1,43 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Col, Container, Row, Table } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
-import { Stuffs } from '../../api/stuff/Stuff';
-import StuffItem from '../components/StuffItem';
 import LoadingSpinner from '../components/LoadingSpinner';
+import Item from '../components/Item';
+import { FoundItems } from '../../api/contact/FoundItems';
 
 /* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
-const ListStuff = () => {
+const ListFoundItem = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
-  const { ready, stuffs } = useTracker(() => {
+  const { ready, contacts } = useTracker(() => {
     // Note that this subscription will get cleaned up
     // when your component is unmounted or deps change.
     // Get access to Stuff documents.
-    const subscription = Meteor.subscribe(Stuffs.userPublicationName);
+    const subscription = Meteor.subscribe(FoundItems.userPublicationName);
     // Determine if the subscription is ready
     const rdy = subscription.ready();
     // Get the Stuff documents
-    const stuffItems = Stuffs.collection.find({}).fetch();
+    const FoundItems = FoundItems.collection.find({}).fetch();
     return {
-      stuffs: stuffItems,
+      contacts: FoundItems,
       ready: rdy,
     };
   }, []);
+
   return (ready ? (
     <Container className="py-3">
       <Row className="justify-content-center">
-        <Col md={7}>
+        <Col>
           <Col className="text-center">
-            <h2>List Stuff</h2>
+            <h2>List Found Item</h2>
           </Col>
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Quantity</th>
-                <th>Condition</th>
-                <th>Edit</th>
-              </tr>
-            </thead>
-            <tbody>
-              {stuffs.map((stuff) => <StuffItem key={stuff._id} stuff={stuff} />)}
-            </tbody>
-          </Table>
+          <Row xs={1} md={2} lg={3} className="g-4">
+            {contacts.map((contact) => (<Col key={contact._id}><Item contact={contact} /></Col>))}
+          </Row>
         </Col>
       </Row>
     </Container>
   ) : <LoadingSpinner />);
 };
 
-export default ListStuff;
+export default ListFoundItem;
