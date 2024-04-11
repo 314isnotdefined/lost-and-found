72a3fb009ac9a547c98e84ea9df2e1f51082639e
darriusdacquel <dacqueld@hawaii.edu>
2024-04-11 11:39:05 -1000

    Revert "Merge branch 'main' into issue-02"
    This reverts commit b1bd48722a0af3986f1e2250f44bdabc3eaddbbd, reversing
    changes made to 1b6d5de87a2d66745c73dc8668020a2fa55ada44.

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -7,7 +7,8 @@ const Landing = () => (
     <Row className="justify-content-center align-items-center w-100">
       <Col xs={12} md={8} className="d-flex flex-column justify-content-center align-items-center text-center">
         <h1>Welcome!</h1>
-        <p>The UH Manoa lost and found app aims to streamline the process of reclaiming lost items for students by offering a centralized platform to report and search for misplaced belongings like wallets and phones. Users can view lists of lost and found items, post details about lost items, and specify their locations.
+        <p>The UH Manoa lost and found app aims to streamline the process of reclaiming lost items for
+          students by offering a centralized platform to report and search for misplaced belongings like wallets and phones. Users can view lists of lost and found items, post details about lost items, and specify their locations.
           Additionally, finders can opt out of receiving messages if items are left
           in designated spaces like the Campus Center and ID Office.
         </p>
