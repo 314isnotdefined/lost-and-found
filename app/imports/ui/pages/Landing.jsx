3b998838b6972af2074325a311c871ed5d15f4e4
Cash Baker <cashbake@hawaii.edu>
2024-04-11 15:41:30 -1000

    Update Landing.jsx

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -8,7 +8,7 @@ const Landing = () => (
       <Col xs={12} md={8} className="d-flex flex-column justify-content-center align-items-center text-center">
         <h1>Welcome!</h1>
         <p>Item Depot offers a centralized system for listing and reclaiming lost and found items at UH Manoa.</p>
-        <p>Users can browse our catalog of user submitted items, contact users with items of interest, or submit a found or lost item for other UHM students to view.
+        <p>Users can browse our catalog of user submitted items, sort items by category, contact users with items of interest, or submit a found or lost item for other UHM students to view.
         </p>
       </Col>
     </Row>
