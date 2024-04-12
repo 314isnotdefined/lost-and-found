d0370b830e69fa76b3fc115b32192f0b42ec8503
Cash Baker <cashbake@hawaii.edu>
2024-04-11 19:14:27 -1000

    changed landing text position

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -4,7 +4,7 @@ import { Col, Container, Row } from 'react-bootstrap';
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
   <Container id="landing-page" fluid className="py-3 d-flex h-100">
-    <Row className="justify-content-center align-items-center w-100">
+    <Row id="center-text" className="justify-content-center align-items-center w-100">
       <Col xs={12} md={8} className="d-flex flex-column justify-content-center align-items-center text-center">
         <h1>Welcome!</h1>
         <p>Item Depot offers a centralized system for listing and reclaiming lost and found items at UH Manoa.</p>
