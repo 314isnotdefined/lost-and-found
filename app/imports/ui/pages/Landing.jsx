b2917f35c41e5499abdbe5b1a65c458b199d2ac1
Cash Baker <cashbake@hawaii.edu>
2024-04-11 15:19:05 -1000

    added back background images

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -7,8 +7,7 @@ const Landing = () => (
     <Row className="justify-content-center align-items-center w-100">
       <Col xs={12} md={8} className="d-flex flex-column justify-content-center align-items-center text-center">
         <h1>Welcome!</h1>
-        <p>The UH Manoa lost and found app aims to streamline the process of reclaiming lost items for
-          students by offering a centralized platform to report and search for misplaced belongings like wallets and phones. Users can view lists of lost and found items, post details about lost items, and specify their locations.
+        <p>Item Depot offers a centralized system for listing and reclaiming lost and found items at UH Manoa. Users can view listings for all lost and found items
           Additionally, finders can opt out of receiving messages if items are left
           in designated spaces like the Campus Center and ID Office.
         </p>
