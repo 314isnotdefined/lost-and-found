93b8720acc4db2e7b0dd1368ef0f886132e88ade
micaiahcape <50351592+micaiahcape@users.noreply.github.com>
2024-04-16 11:06:35 -1000

    Set schema for resolved items
    Added default data and published them

---
+++ app/imports/api/item/ResolvedItemsArchive.js
@@ -0,0 +1,31 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+
+/**
+ * The FoundItemsCollection. It encapsulates state and variable values for stuff.
+ */
+class ResolvedItemsArchiveCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'FoundItemsArchiveCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      uniqueID: String,
+      foundBy: String, // a unique email identified by the user.
+      dateResolved: Date,
+    });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+/**
+ * The singleton instance of the FoundItemsCollection.
+ * @type {FoundItemsCollection}
+ */
+export const ResolvedItemsArchive = new ResolvedItemsArchiveCollection();
