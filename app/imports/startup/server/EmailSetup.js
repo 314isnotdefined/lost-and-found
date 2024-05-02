24c8426095a08aa1be3078356fbb94e706944da7
micaiahcape <50351592+micaiahcape@users.noreply.github.com>
2024-05-02 01:18:58 -1000

    why is meteor email so inconsistent
    darn thing is more inconsistent than my marathon mile splits

--- app/imports/startup/server/EmailSetup.js
+++ app/imports/startup/server/EmailSetup.js
@@ -3,17 +3,21 @@ import { Email } from 'meteor/email';
 import { check } from 'meteor/check';
 
 Meteor.methods({
-  sendEmail(to, from, subject, text) {
+  sendEmail(to, from, subject, html) {
     // Make sure that all arguments are strings.
-    check([to, from, subject, text], [String]);
-
+    check([to, from, subject, html], [String]);
+    console.log(`to: ${to}`);
+    console.log(`from: ${from}`);
+    console.log(`sub: ${subject}`);
+    console.log(`text: ${html}`);
     // Let other method calls from the same client start running, without
     // waiting for the email sending to complete.
     this.unblock();
 
-    Email.sendAsync({ to, from, subject, text }).catch((err) => console.log(err));
+    Email.sendAsync({ to, from, subject, html }).catch((err) => console.log(err));
   },
 });
 
 // set the mail environment variable
 process.env.MAIL_URL = Meteor.settings.emailCredentials;
+console.log(`set process.env.MAIL_URL to ${process.env.MAIL_URL}`);
--- app/imports/ui/components/LostItem.jsx
+++ app/imports/ui/components/LostItem.jsx
@@ -5,13 +5,12 @@ import { AutoForm, ErrorsField, SubmitField, LongTextField, TextField } from 'un
 import { Meteor } from 'meteor/meteor';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import SimpleSchema from 'simpl-schema';
-import nodemailer from 'nodemailer';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Images } from '../../api/item/Images';
 import { Profiles } from '../../api/profile/Profile';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
-
+  // eslint-disable-next-line max-len
 const messageSchema = new SimpleSchema({
   contactInfo: String,
   message: String,
@@ -21,7 +20,7 @@ const bridge = new SimpleSchema2Bridge(messageSchema);
 
 const LostItem = ({ item }) => {
 
-  const { ready, itemImages, userInfo } = useTracker(() => {
+  const { ready, itemImages, ownerInfo, userInfo } = useTracker(() => {
     const subscription = Meteor.subscribe(Images.userPublicationName);
     const userSubscription = Meteor.subscribe(Profiles.userPublicationName);
     const rdy = subscription.ready() && userSubscription.ready();
@@ -42,9 +41,12 @@ const LostItem = ({ item }) => {
       imageArr = fetchedImageData.map(e => e.data);
     }
     const prof = Profiles.collection.find({ email: item.owner }).fetch();
+    const usremail = Meteor.user().username;
+    const usr = Profiles.collection.find({ email: usremail }).fetch();
     return {
       itemImages: [...imageArr],
-      userInfo: prof[0],
+      ownerInfo: prof[0],
+      userInfo: usr[0],
       ready: rdy,
     };
   });
@@ -60,41 +62,20 @@ const LostItem = ({ item }) => {
   }
 
   const submit = ({ data }) => {
-    console.log(JSON.stringify(data));
+    // eslint-disable-next-line max-len
+    /* const htmlText = `<div style='background-color: whitesmoke; font-size: 150%; line-height: 1.3;'><br><div style='justify-content: center; display: flex;'><img src='https://manoa.hawaii.edu/speakers/wp-content/uploads/logo-1-1030x1030.png' alt='item depot logo' width="10%"/><span style='font-size: 200%; margin-top: auto; margin-bottom: auto; padding-left: 2%'>Item Depot</span></div><hr width="60%"><p><b style="color: red">⚠️ Warning: </b> If you did not lose this item, please ignore this email!</p><p>Hi ${ownerInfo.firstName} ${ownerInfo.lastName}, <br><br>${userInfo.firstName} ${userInfo.lastName} has found your item: "${item.itemName}". Here is a message from them indicating the status of the item:</p><div style="padding: 1% 2% 1% 2%; background-color: lightgrey; border-left: 6px solid green"><code>${data.message} </code></div><p>${userInfo.firstName} ${userInfo.lastName} has also provided their contact information should you need to get into contact with them after this.</p><div style="padding: 1% 2% 1% 2%; background-color: lightgrey; border-left: 6px solid green"><code>${data.contactInfo}</code></div><p>If this item has been successfully found, please resolve it and take it off the Item Depot site to prevent confusion. Thank you!</p><p>All the best,</p><p>The Item Depot team (Micaiah, Shayde, Sam, Cash, & Darrius)</p></div>`; */
+
+    // ^^^ AHHH WHAT IS THIS HORROR 😭😭😭
+    // eslint-disable-next-line max-len
+    const htmlTextWithoutCSS = `<p>Hi ${ownerInfo.firstName} ${ownerInfo.lastName}, <br><br>${userInfo.firstName} ${userInfo.lastName} has found your item: "${item.itemName}. The message they sent it ${data.message}".`;
+    console.log(htmlTextWithoutCSS);
     Meteor.call(
       'sendEmail',
       'mchlcape808@gmail.com',
       'itemdepotmsg@outlook.com',
-      'Hello from Meteor!',
-      'This is a test of Email.send.',
+      `${ownerInfo.firstName}, there has been an update on your lost item.`,
+      htmlTextWithoutCSS,
     );
-
-    /* const transporter = nodemailer.createTransport({
-
-      host: 'smtp-mail.outlook.com', // hostname
-      secureConnection: false, // TLS requires secureConnection to be false
-      port: 587, // port for secure SMTP
-      auth: {
-        user: 'itemdepotmsg@outlook.com',
-        pass: '1t3md3p0t',
-      },
-    });
-
-    const mailOptions = {
-      from: 'itemdepotmsg@outlook.com', // sender address
-      to: 'mchlcape808@gmail.com', // list of receivers
-      subject: 'Test Email', // Subject line
-      text: 'Hello world ?', // plain text body
-    };
-
-    transporter.sendMail(mailOptions, function (error, info) {
-
-      if (error) {
-        console.log(`error sending email: ${error}`);
-      } else {
-        console.log(`email sent: ${info.response}`);
-      }
-    }); */
   };
 
   return (
@@ -120,7 +101,7 @@ const LostItem = ({ item }) => {
         <Card.Text><b>Category:</b> {item.category}</Card.Text>
         <Card.Text><b>Description:</b> {item.description}</Card.Text>
         <Card.Text><b>Last Seen At:</b> {item.lastSeen}</Card.Text>
-        <Card.Text><img src={userInfo.image} alt="" style={{ height: '2.5vw', width: '2.5vw', borderRadius: '50%' }} /><span>&nbsp;&nbsp;&nbsp;Owner: {`${userInfo.firstName} ${userInfo.lastName}`}</span></Card.Text>
+        <Card.Text><img src={ownerInfo.image} alt="" style={{ height: '2.5vw', width: '2.5vw', borderRadius: '50%' }} /><span>&nbsp;&nbsp;&nbsp;Owner: {`${ownerInfo.firstName} ${ownerInfo.lastName}`}</span></Card.Text>
         <Button variant="success" style={{ width: '100%' }} onClick={() => toggleContactForm()}>I found this item</Button>
 
         {/* eslint-disable-next-line react/jsx-no-bind */}
@@ -131,7 +112,7 @@ const LostItem = ({ item }) => {
             </Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <p>Please enter a message. This information will be emailed to <b>{`${userInfo.firstName} ${userInfo.lastName}.`}</b></p>
+            <p>Please enter a message. This information will be emailed to <b>{`${ownerInfo.firstName} ${ownerInfo.lastName}.`}</b></p>
             <AutoForm schema={bridge} onSubmit={data => submit(data)}>
               <TextField name="contactInfo" placeholder="email, phone #, instagram, etc... (optional)" label="Your Contact Info" required={false} />
               <LongTextField name="message" placeholder="Indicate details of where you found the item, etc..." required />
