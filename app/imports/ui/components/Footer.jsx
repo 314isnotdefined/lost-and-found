04f4c14f47e164383039ca0447e3ee2510c80264
darriusdacquel <dacqueld@hawaii.edu>
2024-05-06 18:58:52 -1000

    footer update

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
-import { Col, Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const Footer = () => (
-  <footer className="mt-auto py-4" style={{ backgroundColor: 'rgba(52, 58, 64, 0.9)' }}>
+  <footer className="mt-auto py-4" style={{ backgroundColor: 'rgba(52, 58, 64, 0.9)', color: 'white' }}>
     <Container>
-      <Col className="text-center" style={{ fontSize: '14px' }}>
-        ItemDepot is a site that connects students to their lost items through other university students
-        <br />
-        <a href="https://item-depot.github.io/">
-          Organization Page
-        </a>
-        <br />
-        <a href="https://github.com/item-depot/item-depot">
-          Github Repository
-        </a>
-      </Col>
+      <Row>
+        {/* Logo Section */}
+        <Col md={4} className="text-center mb-3 mb-md-0">
+          <img src="/images/uhm-white-seal-nameplate@2x.png" alt="ItemDepot Logo" style={{ width: '200px', height: 'auto' }} />
+        </Col>
+        <Col md={4} className="text-center mb-3 mb-md-0">
+          <h4 style={{ marginBottom: '10px', color: 'white' }}>Item Depot Info</h4>
+          <hr />
+          <div style={{ fontSize: '14px' }}>
+            <a href="https://item-depot.github.io/" className="d-block">Organization Page</a>
+            <a href="https://github.com/item-depot/item-depot">GitHub Repository</a>
+          </div>
+        </Col>
+        <Col md={4} className="text-center">
+          <address>
+            <strong>University of Hawaii at Manoa</strong><br />
+            2500 Campus Road,<br />
+            Honolulu, HI 96822, USA
+          </address>
+        </Col>
+      </Row>
     </Container>
   </footer>
 );
