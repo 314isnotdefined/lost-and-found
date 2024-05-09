dd42e74557176188bc6624313435eec0aa6d6ed9
darriusdacquel <dacqueld@hawaii.edu>
2024-05-08 17:27:48 -1000

    modificaiton on landing page, removed redundant text

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -6,12 +6,24 @@ const Landing = () => (
   <Container id="landing-page" fluid className="py-5 d-flex h-100 align-items-center">
     <Row className="justify-content-center align-items-start w-100">
       <Col xs={12} md={8} className="d-flex flex-column justify-content-center align-items-center text-center">
-        <div style={{ width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <h1 style={{ marginBottom: '20px' }}>Welcome!</h1>
+        <p style={{ marginBottom: '0px' }}>Item Depot offers a centralized system for listing and reclaiming lost and found items at UH Manoa.</p>
+        <p style={{ marginBottom: '30px' }}>Users can browse our catalog of user-submitted items, sort items by category, contact users with items of interest, or submit a found or lost item for other UHM students to view.</p>
+        <div style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+          <Link to="/listlost" style={{ textDecoration: 'none' }}>
+            <Button className="button-custom" style={{ marginRight: '20px' }}>
+              Find Lost Items
+            </Button>
+          </Link>
+          <Link to="/listfound" style={{ textDecoration: 'none' }}>
+            <Button className="button-custom">
+              Find Found Items
+            </Button>
+          </Link>
+        </div>
+        <div style={{ marginTop: '300px', width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
           <div className="text-box white-heading">
             <h2 className="large-heading">Why Use Item Depot?</h2>
-            <p>
-              Item Depot isn&apos;t just for reclaiming lost and found items; it&apos;s a comprehensive resource that facilitates community interaction and support.
-            </p>
             <p>
               <span className="strong-highlight">Submit a Lost Item:</span> Lost something? Fill out our
               <Link to="/add" className="hyperlink"> Submit Lost Item Form</Link>.
@@ -32,18 +44,6 @@ const Landing = () => (
               <span className="strong-highlight">Easy Navigation and Robust Features:</span> Our platform is designed to be user-friendly,
               ensuring a smooth experience for all users, whether reporting lost items or helping others recover their belongings.
             </p>
-          </div>
-          <div style={{ display: 'flex', justifyContent: 'center', width: '100%', marginTop: '50px' }}>
-            <Link to="/listlost" style={{ textDecoration: 'none' }}>
-              <Button className="button-custom" style={{ marginRight: '20px' }}>
-                Find Lost Items
-              </Button>
-            </Link>
-            <Link to="/listfound" style={{ textDecoration: 'none' }}>
-              <Button className="button-custom">
-                Find Found Items
-              </Button>
-            </Link>
           </div>
           <h2 className="campus-map-heading white-heading" style={{ marginTop: '200px' }}>Campus Map</h2>
           <a href="https://map.hawaii.edu/manoa/" target="_blank" rel="noopener noreferrer" className="image-link">
